ac21b5b726c54285671e1d10f5da7f9eddabb69b
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-11-27 13:50:06 +0900

    拡張メソッドの定義にC#14の構文を使うように変更しました

--- src/Maris.Logging.Testing/Xunit/TestLoggerServiceCollectionExtensions.cs
+++ src/Maris.Logging.Testing/Xunit/TestLoggerServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Maris.Logging.Testing.Xunit;
+﻿using System.Diagnostics.CodeAnalysis;
+using Maris.Logging.Testing.Xunit;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -6,38 +7,44 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// <summary>
 ///  <see cref="TestLoggerManager"/> で管理できる <see cref="ILogger"/> を登録する処理を提供します。
 /// </summary>
+[SuppressMessage(
+    category: "StyleCop.CSharp.ReadabilityRules",
+    checkId: "SA1101:PrefixLocalCallsWithThis",
+    Justification = "StyleCop bug. see: https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3954")]
 public static class TestLoggerServiceCollectionExtensions
 {
-    /// <summary>
-    ///  テストで使用するログ出力の構成をします。
-    /// </summary>
-    /// <param name="services">ログ出力の構成を行う <see cref="IServiceCollection"/> 。</param>
-    /// <param name="loggerManager">Xunit のテストクラスで使用する <see cref="TestLoggerManager"/> 。</param>
-    /// <returns><see cref="IServiceCollection"/> 。</returns>
-    /// <exception cref="ArgumentNullException">
-    ///  <list type="bullet">
-    ///   <item><paramref name="services"/> が <see langword="null"/> です。</item>
-    ///   <item><paramref name="loggerManager"/> が <see langword="null"/> です。</item>
-    ///  </list>
-    /// </exception>
-    public static IServiceCollection AddTestLogging(this IServiceCollection services, TestLoggerManager loggerManager)
+    extension(IServiceCollection services)
     {
-        if (services is null)
+        /// <summary>
+        ///  テストで使用するログ出力の構成をします。
+        /// </summary>
+        /// <param name="loggerManager">Xunit のテストクラスで使用する <see cref="TestLoggerManager"/> 。</param>
+        /// <returns><see cref="IServiceCollection"/> 。</returns>
+        /// <exception cref="ArgumentNullException">
+        ///  <list type="bullet">
+        ///   <item><see cref="IServiceCollection"/> が <see langword="null"/> です。</item>
+        ///   <item><paramref name="loggerManager"/> が <see langword="null"/> です。</item>
+        ///  </list>
+        /// </exception>
+        public IServiceCollection AddTestLogging(TestLoggerManager loggerManager)
         {
-            throw new ArgumentNullException(nameof(services));
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (loggerManager is null)
+            {
+                throw new ArgumentNullException(nameof(loggerManager));
+            }
+
+            services.AddLogging(builder =>
+            {
+                builder.AddXunitLogging(loggerManager);
+                builder.AddFakeLogging(loggerManager);
+            });
+
+            return services;
         }
-
-        if (loggerManager is null)
-        {
-            throw new ArgumentNullException(nameof(loggerManager));
-        }
-
-        services.AddLogging(builder =>
-        {
-            builder.AddXunitLogging(loggerManager);
-            builder.AddFakeLogging(loggerManager);
-        });
-
-        return services;
     }
 }
