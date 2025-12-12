62b1af84ecac6ab1a1d5646c9ba2b96aa78634d7
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-12-12 16:20:43 +0900

    EnumExtensionsのGetDisplayNameメソッドのXMLコメントを修正し、使用例を追加

--- src/Maris.ComponentModel.Annotations/EnumExtensions.cs
+++ src/Maris.ComponentModel.Annotations/EnumExtensions.cs
@@ -11,14 +11,34 @@ public static class EnumExtensions
     extension<TEnum>(TEnum value) where TEnum : struct, Enum
     {
         /// <summary>
-        ///  列挙体の値に付与された <see cref="DisplayAttribute"/> の名前を取得します。
-        ///  属性が存在しない場合は列挙体の名前を返します。
+        ///  列挙型の値に付与された <see cref="DisplayAttribute"/> の名前を取得します。
+        ///  属性が存在しない場合は列挙型の名前を返します。
         /// </summary>
-        /// <returns>Display 名または Enum 名。</returns>
+        /// <returns>列挙型の表示名。</returns>
         /// <example>
+        ///  <para>
+        ///   列挙型の定義例と使用例を次に示します。
+        ///  </para>
         ///  <code language="csharp">
         ///   <![CDATA[
-        ///    aaa;
+        ///    using Maris.ComponentModel;
+        ///
+        ///    public enum Status
+        ///    {
+        ///        [Display(Name = "Preparation is ready.")]
+        ///        Ready = 1,
+        ///
+        ///        [Display(
+        ///            ResourceType = typeof(EnumExtensionsTestResources),
+        ///            Name = nameof(EnumExtensionsTestResources.InProgress))]
+        ///        InProgress = 2,
+        ///
+        ///        Done = 3,
+        ///    }
+        ///
+        ///    Console.WriteLine(Status.Ready.GetDisplayName());      // Output: Preparation is ready.
+        ///    Console.WriteLine(Status.InProgress.GetDisplayName()); // Output: Work is in progress.
+        ///    Console.WriteLine(Status.Done.GetDisplayName());       // Output: Done
         ///   ]]>
         ///  </code>
         /// </example>
